b5b35ef2baca4e16bfd7a9fef79df8ac3df1ac99
Klaudia Barańska <klaudiabaranska11@gmail.com>
2020-11-20 17:52:06 +0100

    Loading image

---
+++ DermaHacker/DermaHacker/Models/ImageAnalyse/ImagePreprocessing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DermaHacker.Models.ImagePreprocessing
+{
+    class ImagePreprocessing
+    {
+        public Image loadImage()
+        {
+            string path = "C:\\Users\\Wika\\Desktop\\Kej\\hackaton\\Cases_Thermography\\Case1A.jpg";
+            var image = new Image { Source = path };
+            return image;
+        }
+    }
+}
