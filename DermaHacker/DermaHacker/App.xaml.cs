614b92dd8295990e5ac302181a1770368ed02c0a
Anna Dębowska <anna.debowska@apagroup.pl>
2020-11-20 17:21:57 +0100

    Item menu

--- DermaHacker/DermaHacker/App.xaml.cs
+++ DermaHacker/DermaHacker/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DermaHacker.Services;
+using DermaHacker.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +8,13 @@ namespace DermaHacker
 {
     public partial class App : Application
     {
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
---
+++ DermaHacker/DermaHacker/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DermaHacker.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DermaHacker.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
